98a69b883d6a5b3ef7891e0061d3f4962ecfcdbc
Ramya <ramyakpass@gmail.com>
2026-03-10 22:17:58 -0400

    updated

--- SpringAICode/SpringEcom/src/main/java/com/ecom/SpringEcom/controller/HelloController.java
+++ SpringAICode/SpringEcom/src/main/java/com/ecom/SpringEcom/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.telusko.SpringEcom.controller;
+package com.ecom.SpringEcom.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String greet(){
-        return "Welcome to Telusko";
+        return "Welcome to World";
     }
 
 }
--- SpringAICode/SpringEcom/src/main/java/com/ecom/SpringEcom/service/AiImageGeneratorService.java
+++ SpringAICode/SpringEcom/src/main/java/com/ecom/SpringEcom/service/AiImageGeneratorService.java
@@ -1,4 +1,4 @@
-package com.telusko.SpringEcom.service;
+package com.ecom.SpringEcom.service;
 
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
